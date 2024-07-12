34d1e0af3809de50622ffdcb6faf2f798ed20d26
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-07-12 10:00:16 +0700

    feat: Added new api for send test notification

---
+++ src/app/api/send-notification/route.ts
@@ -0,0 +1,38 @@
+import admin from 'firebase-admin'
+import type { Message } from 'firebase-admin/messaging'
+import { NextResponse } from 'next/server'
+
+if (!admin.apps.length) {
+	const serviceAccount = require('@/secrets/firebase-admin-sdk.json')
+
+	admin.initializeApp({
+		credential: admin.credential.cert(serviceAccount),
+	})
+}
+
+export async function POST(request: Request) {
+	try {
+		const result = await request.json()
+
+		const message: Message = {
+			token: result.token,
+			data: {
+				title: result.title,
+				...result.body,
+			},
+		}
+
+		const messageResult = await admin.messaging().send(message)
+
+		return NextResponse.json({
+			success: true,
+			message: 'Notification sent!',
+			result: messageResult,
+		})
+	} catch (error) {
+		return NextResponse.json({
+			success: false,
+			error,
+		})
+	}
+}
