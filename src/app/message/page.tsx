9340df2e490b1308e6aa97eab7cb63f03db784e4
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-07-12 10:00:29 +0700

    feat: Added message receiver page

---
+++ src/app/message/page.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { MessagePayload, onMessage } from 'firebase/messaging'
+import { useEffect, useState } from 'react'
+
+import { useFirebaseContext } from '@/context/useFirebaseContext'
+
+const Message = () => {
+	const [result, setResult] = useState<MessagePayload[]>([])
+
+	const { fcmToken, messaging } = useFirebaseContext()
+
+	useEffect(() => {
+		const unsubscribe = onMessage(messaging, (payload) => {
+			console.log('Here is the payload from FCM', payload)
+
+			setResult((prev) => [...prev, payload])
+		})
+
+		return () => {
+			unsubscribe()
+		}
+	}, [messaging])
+
+	return (
+		<div className="flex flex-col gap-8 mx-auto min-w-[768px] max-w-full min-h-screen p-8">
+			<h1 className="text-center text-2xl font-bold">Here is the message result</h1>
+
+			<p>Current FCM token: {fcmToken}</p>
+
+			{result.length === 0 ? null : <pre>{JSON.stringify(result, null, 2)}</pre>}
+		</div>
+	)
+}
+
+export default Message
