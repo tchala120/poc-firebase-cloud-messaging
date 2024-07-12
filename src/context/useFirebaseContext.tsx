cefff3a7312248a873ff41bd2c7ab73fa971a3bb
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-07-12 12:22:07 +0700

    feat: Added auth flow into firebase context

--- src/context/useFirebaseContext.tsx
+++ src/context/useFirebaseContext.tsx
@@ -1,14 +1,18 @@
 'use client'
 
-import { useQuery } from '@tanstack/react-query'
-import { Messaging, getToken } from 'firebase/messaging'
-import { ReactNode, createContext, useContext, useState } from 'react'
+import { GoogleAuthProvider, User, onAuthStateChanged, signInWithPopup } from 'firebase/auth'
+import { Messaging } from 'firebase/messaging'
+import { type ReactNode, createContext, useContext, useEffect, useState } from 'react'
 
-import { messaging } from '@/setup/firebase'
+import { auth, getFCMToken } from '@/setup/firebase'
 
 interface FirebaseContextData {
-	messaging: Messaging
+	messaging: Messaging | null
 	fcmToken: string | null
+	isLoggedIn: boolean
+	user: User | null
+	signIn: () => Promise<any>
+	signOut: VoidFunction
 }
 
 const FirebaseContext = createContext<any>(null)
@@ -17,51 +21,145 @@ interface FirebaseProviderProps {
 	children: ReactNode
 }
 
-export const FirebaseProvider = ({ children }: FirebaseProviderProps) => {
-	const [fcmMessaging, setFCMMessaging] = useState<Messaging | null>(null)
+const googleAuthProvider = new GoogleAuthProvider()
+googleAuthProvider.setCustomParameters({
+	prompt: 'select_account',
+})
 
+export const FirebaseProvider = ({ children }: FirebaseProviderProps) => {
+	const [loading, setLoading] = useState(true)
+	const [firebaseUser, setFirebaseUser] = useState<User | null>(null)
+
+	const [fcmMessaging, setFCMMessaging] = useState<Messaging | null>(null)
 	const [fcmToken, setFCMToken] = useState<string | null>(null)
 
-	useQuery({
-		queryKey: ['initialize-firebase-cloud-messaging'],
-		queryFn: async () => {
-			try {
-				const fcmMessaging = await messaging()
+	const sendTokenToServer = async () => {
+		const response = await getFCMToken()
 
-				if (fcmMessaging == null) {
-					throw Error('Firebase Cloud Messaging is not supported')
-				}
+		if (response == null) {
+			return
+		}
 
-				setFCMMessaging(fcmMessaging)
+		const userToken = await auth.currentUser?.getIdToken()
 
-				const permission = await Notification.requestPermission()
+		if (userToken == null) {
+			return
+		}
 
-				if (permission === 'granted') {
-					const result = await getToken(fcmMessaging, {
-						vapidKey: process.env.NEXT_PUBLIC_FIREBASE_CLOUD_MESSAGE_PUBLIC_KEY,
+		const result = await fetch('/api/fcm-token', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: userToken,
+			},
+			body: JSON.stringify({
+				token: response.token,
+			}),
+		})
+
+		setFCMMessaging(response.messaging)
+		setFCMToken(response.token)
+
+		return result
+	}
+
+	const signIn = async () =>
+		new Promise((resolve, reject) => {
+			signInWithPopup(auth, googleAuthProvider)
+				.then((result) => {
+					console.log('Result', result)
+
+					const credential = GoogleAuthProvider.credentialFromResult(result)
+
+					if (credential == null) {
+						reject({
+							code: 'credential-not-found',
+							message: 'Credential not found',
+						})
+
+						return
+					}
+
+					const token = credential.accessToken
+
+					const user = result.user
+
+					sendTokenToServer().then((response) => {
+						if (response == null) {
+							return
+						}
+
+						return resolve({
+							user,
+							token,
+						})
 					})
+				})
+				.catch((error) => {
+					const errorCode = error.code
+					const errorMessage = error.message
 
-					setFCMToken(result)
+					const email = error.customData.email
 
-					return result
-				}
+					const credential = GoogleAuthProvider.credentialFromError(error)
 
-				return null
-			} catch (error) {
-				console.log('Error from messaging', error)
+					reject({
+						code: errorCode,
+						message: errorMessage,
+						data: {
+							email,
+							credential,
+						},
+					})
+				})
+				.finally(() => setLoading(false))
+		})
 
-				return null
+	const signOut = async () => {
+		try {
+			await auth.signOut()
+
+			setFirebaseUser(null)
+		} catch (error) {
+			console.log('Error signing out user', error)
+		}
+	}
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, async (user) => {
+			if (user == null) {
+				setLoading(false)
+
+				return
 			}
-		},
-	})
 
-	if (fcmMessaging == null) {
-		return null
+			sendTokenToServer()
+
+			setFirebaseUser(user)
+
+			setLoading(false)
+		})
+
+		return () => {
+			unsubscribe()
+		}
+	}, [])
+
+	if (loading) {
+		return (
+			<div className="flex justify-center items-center min-h-screen">
+				<h1 className="m-0 text-center font-bold text-4xl">Loading...</h1>
+			</div>
+		)
 	}
 
 	const value: FirebaseContextData = {
 		messaging: fcmMessaging,
 		fcmToken,
+		isLoggedIn: firebaseUser != null,
+		user: firebaseUser,
+		signIn,
+		signOut,
 	}
 
 	return <FirebaseContext.Provider value={value}>{children}</FirebaseContext.Provider>
