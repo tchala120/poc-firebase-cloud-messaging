7016dc708b111a716161772e4d54b2c88dad18d9
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-07-12 09:51:21 +0700

    feat: Added env type safe

---
+++ src/environtment.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="node" />
+
+declare namespace NodeJS {
+	interface ProcessEnv {
+		NODE_ENV: 'development' | 'production'
+
+		/**
+		 * Firebase configuration
+		 */
+
+		NEXT_PUBLIC_FIREBASE_API_KEY: string
+		NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string
+		NEXT_PUBLIC_FIREBASE_PROJECT_ID: string
+		NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string
+		NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string
+		NEXT_PUBLIC_FIREBASE_APP_ID: string
+		NEXT_PUBLIC_FIREBASE_CLOUD_MESSAGE_PUBLIC_KEY: string
+	}
+}
