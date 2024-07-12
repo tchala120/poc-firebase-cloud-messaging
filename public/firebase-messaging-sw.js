fab87afc0e86d8bf17bc8bd7e87c98745aa08948
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-07-12 12:24:12 +0700

    fix: Replaced the key with env

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyBaw__5XKVNy1C0U2HVUgDEbete0gRQGjs',
-	authDomain: 'poc-fcm-2bb17.firebaseapp.com',
-	projectId: 'poc-fcm-2bb17',
-	storageBucket: 'poc-fcm-2bb17.appspot.com',
-	messagingSenderId: '913839611916',
-	appId: '1:913839611916:web:5c21377622fd2f2469ce29',
-	measurementId: 'BMRemlymv_nUuDfjaw282mVs25SSr3Srr8xiQaoI35brGYOXS6cbVwk2ag0pCPXub00dMNh6s3Zqay8oycg3Aws',
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_CLOUD_MESSAGE_PUBLIC_KEY,
 }
 
 firebase.initializeApp(firebaseConfig)
