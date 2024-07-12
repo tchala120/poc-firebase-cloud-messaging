9dc1e2780b6a7af4081a3aab9568db04369016df
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-07-12 09:59:51 +0700

    feat: Added send test noti button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+'use client'
+
+import { SendTestNotificationButton } from '@/components/SendTestNotificationButton'
+
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<main className="flex min-h-screen flex-col items-center justify-center gap-8 p-24">
 			<h1>PoC Firebase Cloud Messaging</h1>
+
+			<SendTestNotificationButton />
 		</main>
 	)
 }
