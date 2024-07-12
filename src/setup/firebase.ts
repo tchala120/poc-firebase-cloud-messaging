c132b9d21f450614630156b2b849868554d8abf9
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-07-12 12:22:20 +0700

    feat: Added get fcm token

--- src/setup/firebase.ts
+++ src/setup/firebase.ts
@@ -1,5 +1,6 @@
 import { getApp, getApps, initializeApp } from 'firebase/app'
-import { getMessaging, isSupported } from 'firebase/messaging'
+import { getAuth } from 'firebase/auth'
+import { getMessaging, getToken, isSupported } from 'firebase/messaging'
 
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -15,5 +16,34 @@ export const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getA
 export const messaging = async () => {
 	const supported = await isSupported()
 
+	console.log('Is browser supported', supported)
+
 	return supported ? getMessaging(app) : null
+}
+
+export const auth = getAuth(app)
+
+export const getFCMToken = async () => {
+	try {
+		const fcmMessaging = await messaging()
+
+		if (fcmMessaging == null) {
+			throw Error('Firebase Cloud Messaging is not supported')
+		}
+
+		const permission = await Notification.requestPermission()
+
+		if (permission === 'granted') {
+			const token = await getToken(fcmMessaging, {
+				vapidKey: process.env.NEXT_PUBLIC_FIREBASE_CLOUD_MESSAGE_PUBLIC_KEY,
+			})
+
+			return {
+				token,
+				messaging: fcmMessaging,
+			}
+		}
+	} catch (error) {
+		console.log('Error from messaging', error)
+	}
 }
