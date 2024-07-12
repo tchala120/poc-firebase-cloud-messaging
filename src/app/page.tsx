4d54f4de05f54da7e55511d29a4fbdaa87282327
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-07-12 11:42:52 +0700

    feat: Added state log in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 'use client'
 
+import { Button } from '@/components/Button'
 import { SendTestNotificationButton } from '@/components/SendTestNotificationButton'
 
+import { useFirebaseContext } from '@/context/useFirebaseContext'
+
 export default function Home() {
+	const { isLoggedIn, signIn, signOut } = useFirebaseContext()
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center gap-8 p-24">
 			<h1>PoC Firebase Cloud Messaging</h1>
 
-			<SendTestNotificationButton />
+			{isLoggedIn ? (
+				<>
+					<SendTestNotificationButton />
+
+					<Button onClick={() => signOut()}>Sign Out</Button>
+				</>
+			) : (
+				<Button onClick={() => signIn()}>Sign In</Button>
+			)}
 		</main>
 	)
 }
