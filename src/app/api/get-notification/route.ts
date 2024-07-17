e5be551a5082da57942e050af5da41725592c5d7
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-07-17 13:35:14 +0700

    feat: Added api get notification

---
+++ src/app/api/get-notification/route.ts
@@ -0,0 +1,81 @@
+import admin from 'firebase-admin'
+import { DecodedIdToken } from 'firebase-admin/auth'
+import { jwtDecode } from 'jwt-decode'
+import { NextResponse } from 'next/server'
+
+if (!admin.apps.length) {
+	const serviceAccount = require('@/secrets/firebase-admin-sdk.json')
+
+	admin.initializeApp({
+		credential: admin.credential.cert(serviceAccount),
+	})
+}
+
+const db = admin.firestore()
+
+export async function GET(request: Request) {
+	try {
+		const authorizationToken = request.headers.get('Authorization')
+
+		if (authorizationToken == null) {
+			return NextResponse.json(
+				{
+					success: false,
+					error: 'Unauthorized access',
+				},
+				{
+					status: 401,
+				},
+			)
+		}
+
+		const decodedToken = jwtDecode<DecodedIdToken>(authorizationToken)
+
+		const notificationRef = db.collection('/notifications')
+
+		const listNotifications = await notificationRef.where('to', '==', decodedToken.email).get()
+
+		if (listNotifications.empty) {
+			return NextResponse.json(
+				{
+					status: 200,
+					message: 'Successfully',
+					data: [],
+				},
+				{
+					status: 200,
+				},
+			)
+		}
+
+		const notifications = listNotifications.docs.map((doc) => {
+			const data = doc.data()
+
+			return {
+				id: doc.id,
+				title: data.title,
+				description: data.description,
+				imageUrl: data.imageUrl,
+				link: data.link,
+				createdAt: data.createdAt,
+			}
+		})
+
+		return NextResponse.json(
+			{
+				status: 200,
+				message: 'Successfully',
+				data: notifications,
+			},
+			{
+				status: 200,
+			},
+		)
+	} catch (error) {
+		console.log('Error getting list notifications: ', error)
+
+		return NextResponse.json(error, {
+			status: 500,
+		})
+	}
+}
