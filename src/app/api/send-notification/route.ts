d78bf067f1af384b34534ace0a3a8c80c98a1c26
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-07-18 10:08:06 +0700

    faet: Adddd process to add notification into database

--- src/app/api/send-notification/route.ts
+++ src/app/api/send-notification/route.ts
@@ -10,6 +10,8 @@ if (!admin.apps.length) {
 	})
 }
 
+const db = admin.firestore()
+
 export async function POST(request: Request) {
 	try {
 		const result = await request.json()
@@ -27,6 +29,19 @@ export async function POST(request: Request) {
 		}
 
 		const messageResult = await admin.messaging().send(message)
+
+		const listUsers = await db.collection('/fcm-tokens').where('token', '==', result.token).get()
+		const user = listUsers.docs[0].data()
+
+		await db.collection('/notifications').add({
+			id: messageResult,
+			title: result.title,
+			description: result.description,
+			imageURL: result.imageUrl,
+			link: result.link,
+			to: user.email,
+			status: 'unread',
+		})
 
 		return NextResponse.json({
 			success: true,
